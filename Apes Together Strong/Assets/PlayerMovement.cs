3d3e51f1ec7ca4dd2fd444a7285b11594809defa
ba <brandon.ardoin@selu.edu>
2021-02-16 20:16:50 -0600

    player animation
    player animations and animation controller

--- Apes Together Strong/Assets/PlayerMovement.cs
+++ Apes Together Strong/Assets/PlayerMovement.cs
@@ -11,6 +11,8 @@ public class PlayerMovement : MonoBehaviour
     public float stamina = 20;
     public float maxStamina = 25;
 
+    public Animator animator;
+
     Vector2 movement;
 
     void Update()
@@ -19,6 +21,11 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        //Sprint and Stamina
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             sprinting = true;
